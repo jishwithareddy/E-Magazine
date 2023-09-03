2aea8c56d3ae398bc3e4afd4520fb051e6f058d5
jishwithareddy <97454760+jishwithareddy@users.noreply.github.com>
2023-09-03 23:17:12 +0530

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.e.magazine;
+
+public class Start {
+    
+    public static void main(String []args){
+        
+        onboarding st = new onboarding();
+        st.setVisible(true);
+        
+    }
+    
+}
